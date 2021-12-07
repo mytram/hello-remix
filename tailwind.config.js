7874f208a9cdfebef7ce0318e6a1dc28455383d2
marty tie ma <tie.ma@anz.com>
2021-12-07 21:48:46 +1100

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./app/**/*.{ts,tsx}"],
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: [
+  ]
+};
+
